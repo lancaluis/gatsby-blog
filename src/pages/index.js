9dca8e3978de034d1d4db24ee014b0b9fa9d3e0a
Luís Lança <luis_lanca@outlook.com>
2020-08-13 10:23:58 -0300

    Change CSS imports
    Signed-off-by: Luís Lança <luis_lanca@outlook.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Layout from "../components/Layout"
 import SocialLinks from "../components/SocialLinks"
 import { translate } from "../i18n/translate"
 
-import * as S from "../styles/home"
+import * as S from "./styled"
 
 const ProfileImage = () => {
   const { profileImage } = useStaticQuery(graphql`
